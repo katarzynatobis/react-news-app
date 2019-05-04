42bad204d92d1984544ab5311c577015f5608c4a
katawes <katawes@gmail.com>
2019-05-04 18:00:41 +0200

    Updates

---
+++ precache-manifest.a8c71922b487c6a295f9f4d22f170221.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6eeb3b5f05944cd4291b776f2ad7741",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "b4f3f3b84576ef19022e",
+    "url": "/react-news-app/static/css/main.850c2577.chunk.css"
+  },
+  {
+    "revision": "c157b6b4a2f3c1dc4d64",
+    "url": "/react-news-app/static/js/2.ba00711e.chunk.js"
+  },
+  {
+    "revision": "b4f3f3b84576ef19022e",
+    "url": "/react-news-app/static/js/main.9a99913d.chunk.js"
+  },
+  {
+    "revision": "9a5a725e2deca7c9e741",
+    "url": "/react-news-app/static/js/runtime~main.432a7ef3.js"
+  },
+  {
+    "revision": "06b8e91b42ae414416b03875db93412e",
+    "url": "/react-news-app/static/media/fontello.06b8e91b.woff2"
+  },
+  {
+    "revision": "7dc49073965e882e751d8beffb9e6263",
+    "url": "/react-news-app/static/media/fontello.7dc49073.svg"
+  },
+  {
+    "revision": "81331d956fda6daed5c036c5ca687618",
+    "url": "/react-news-app/static/media/fontello.81331d95.woff"
+  },
+  {
+    "revision": "be372535215c456431bb10f4fe95fae8",
+    "url": "/react-news-app/static/media/fontello.be372535.eot"
+  },
+  {
+    "revision": "ec4a0f9c911f66e80574127e55f668d8",
+    "url": "/react-news-app/static/media/fontello.ec4a0f9c.ttf"
+  }
+]);
